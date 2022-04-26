17b9be2c8baed5414a74610ef9aca96fb138efe0
Aadi Poddar <aadipoddarmail@gmail.com>
2022-04-26 22:10:45 +0530

    Create Post Functionality

--- src/pages/r/[communityId]/submit.tsx
+++ src/pages/r/[communityId]/submit.tsx
@@ -1,4 +1,27 @@
+import { Box, Text } from '@chakra-ui/react'
+import Head from 'next/head'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import PageContent from '../../../components/Layout/PageContent'
+import NewPostForm from '../../../components/Posts/NewPostForm'
+import { auth } from '../../../firebase/clientApp'
+
 const SubmitPostPage: React.FC = () => {
-    return <div>Submit page</div>
+    const [user] = useAuthState(auth)
+    return (
+        <>
+            <Head>
+                <title>Create a Post</title>
+            </Head>
+            <PageContent>
+                <>
+                    <Box p='14px 0px' borderBottom='1px solid' borderColor='white'>
+                        <Text>Create a post</Text>
+                    </Box>
+                    {user && <NewPostForm user={user} />}
+                </>
+                <>{/* About */}</>
+            </PageContent>
+        </>
+    )
 }
 export default SubmitPostPage
