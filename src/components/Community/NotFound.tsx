564ca69fcf08a62e0e4e3e84f54d4fbdf6b6b0dd
Aadi Poddar <aadipoddarmail@gmail.com>
2022-04-26 21:02:52 +0530

    Community and Not Found Page

---
+++ src/components/Community/NotFound.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Flex, Button } from '@chakra-ui/react'
+import Link from 'next/link'
+import Head from 'next/head'
+
+const CommunityNotFound: React.FC = () => {
+    return (
+        <>
+            <Head>
+                <title>Community Not Found</title>
+            </Head>
+            <Flex
+                direction='column'
+                justifyContent='center'
+                alignItems='center'
+                minHeight='60vh'
+            >
+                Sorry, that community does not exist or has been banned
+                <Link href='/'>
+                    <Button mt={4}>GO HOME</Button>
+                </Link>
+            </Flex>
+        </>
+    )
+}
+export default CommunityNotFound
---
+++ src/pages/r/[communityId]/index.tsx
@@ -0,0 +1,66 @@
+import { doc, getDoc } from 'firebase/firestore'
+import { GetServerSidePropsContext } from 'next'
+import React from 'react'
+import { Community } from '../../../atoms/communitiesAtom'
+import { firestore } from '../../../firebase/clientApp'
+import safeJsonStringify from 'safe-json-stringify'
+import NotFound from '../../../components/Community/NotFound'
+import Header from '../../../components/Community/Header'
+import PageContent from '../../../components/Layout/PageContent'
+import Head from 'next/head'
+
+type CommunityPageProps = {
+    communityData: Community
+}
+
+const CommunityPage: React.FC<CommunityPageProps> = ({ communityData }) => {
+    console.log('here is data', communityData)
+
+    if (!communityData) {
+        return <NotFound />
+    }
+
+    return (
+        <>
+            <Head>
+                <title>{communityData.id}</title>
+            </Head>
+            <Header communityData={communityData} />
+            <PageContent>
+                <>
+                    <div>LHS</div>
+                </>
+                <>
+                    <div>RHS</div>
+                </>
+            </PageContent>
+        </>
+    )
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+    // get community data and pass it to client
+    try {
+        const communityDocRef = doc(
+            firestore,
+            'communities',
+            context.query.communityId as string
+        )
+        const communityDoc = await getDoc(communityDocRef)
+
+        return {
+            props: {
+                communityData: communityDoc.exists()
+                    ? JSON.parse(
+                        safeJsonStringify({ id: communityDoc.id, ...communityDoc.data() })
+                    )
+                    : '',
+            },
+        }
+    } catch (error) {
+        // Could add error page here
+        console.log('getServerSideProps error', error)
+    }
+}
+
+export default CommunityPage
