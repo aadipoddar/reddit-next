d0a18dafce9061b06281b3b0ab0d128c2ea42e35
Aadi Poddar <aadipoddarmail@gmail.com>
2022-05-01 12:18:09 +0530

    Comment functionality

--- src/pages/r/[communityId]/submit.tsx
+++ src/pages/r/[communityId]/submit.tsx
@@ -1,15 +1,15 @@
 import { Box, Text } from '@chakra-ui/react'
 import Head from 'next/head'
 import { useAuthState } from 'react-firebase-hooks/auth'
-import { useRecoilValue } from 'recoil'
-import { communityState } from '../../../atoms/communitiesAtom'
+import About from "../../../components/Community/About"
 import PageContent from '../../../components/Layout/PageContent'
 import NewPostForm from '../../../components/Posts/NewPostForm'
 import { auth } from '../../../firebase/clientApp'
+import useCommunityData from "../../../hooks/useCommunityData"
 
 const SubmitPostPage: React.FC = () => {
     const [user] = useAuthState(auth)
-    const communityStateValue = useRecoilValue(communityState)
+    const { communityStateValue } = useCommunityData();
     console.log('COMMUNITY', communityStateValue)
 
     return (
@@ -24,7 +24,11 @@ const SubmitPostPage: React.FC = () => {
                     </Box>
                     {user && <NewPostForm user={user} />}
                 </>
-                <>{/* About */}</>
+                <>
+                    {communityStateValue.currentCommunity && (
+                        <About communityData={communityStateValue.currentCommunity} />
+                    )}
+                </>
             </PageContent>
         </>
     )
