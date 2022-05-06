c29542a140777e5f8b2af14ff06c6e32c996982f
Aadi Poddar <aadipoddarmail@gmail.com>
2022-05-06 13:29:33 +0530

    Home Page

--- src/hooks/useCommunityData.tsx
+++ src/hooks/useCommunityData.tsx
@@ -19,7 +19,7 @@ import {
 import { auth, firestore } from '../firebase/clientApp'
 
 const useCommunityData = () => {
-    const [user, loadingUser] = useAuthState(auth)
+    const [user] = useAuthState(auth)
     const router = useRouter()
     const [communityStateValue, setCommunityStateValue] =
         useRecoilState(communityState)
@@ -60,6 +60,7 @@ const useCommunityData = () => {
             setCommunityStateValue((prev) => ({
                 ...prev,
                 mySnippets: snippets as CommunitySnippet[],
+                snippetsFetched: true,
             }))
         } catch (error: any) {
             console.log('getMySnippets error', error)
@@ -158,6 +159,7 @@ const useCommunityData = () => {
             setCommunityStateValue((prev) => ({
                 ...prev,
                 mySnippets: [],
+                snippetsFetched: false,
             }))
             return
         }
